bd5b0c54e2d3097ba6aec10f684870c5aa922d54
awsragook2 <awsragook2@gmail.com>
2025-11-13 12:41:09 -0500

    added small comments to check ne stages

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ module "sandbox" {
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
+    change_requested_by = "HashiCorp Learn 1"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
